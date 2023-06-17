0fa88959df2bb0c5698f4702e7748088d653e5eb
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-06-17 19:41:38 +0530

    skills section

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,11 +1,13 @@
 import About from './sections/About'
 import Hero from './sections/Hero'
+import Skills from './sections/Skills'
 
 function Home() {
     return (
         <div>
             <Hero />
             <About />
+            <Skills />
         </div>
     )
 }
---
+++ src/components/sections/Skills.tsx
@@ -0,0 +1,37 @@
+function Skills() {
+    return (
+        <div className="bg-[#778DA9] px-[20px] md:px-[50px] lg:px-[100px] py-[40px] md:py-[50px] lg:py-[100px]">
+            <div className="dev-toolkit-heading pb-[40px] md:pb-[60px] text-[32px] md:text-[48px]">
+                The Developer’s Toolkit
+            </div>
+            <div className="grid grid-cols-1 gap-[40px] md:grid-cols-3">
+                <div className="flex flex-col gap-[10px]">
+                    <div className="dev-toolkit-sub-heading">Languages</div>
+                    <div className="dev-toolkit-desc">Java, Python, JavaScript, Ruby – you name it, he's fluent</div>
+                </div>
+                <div className="flex flex-col gap-[10px]">
+                    <div className="dev-toolkit-sub-heading">Frameworks</div>
+                    <div className="dev-toolkit-desc">Node.js, React, Django, Rails – he’s the architect</div>
+                </div>
+                <div className="flex flex-col gap-[10px]">
+                    <div className="dev-toolkit-sub-heading">Databases</div>
+                    <div className="dev-toolkit-desc">MySQL, PostgreSQL, MongoDB – he’s got it stored</div>
+                </div>
+                <div className="flex flex-col gap-[10px]">
+                    <div className="dev-toolkit-sub-heading">DevOps</div>
+                    <div className="dev-toolkit-desc">AWS, Docker, Kubernetes – he’s got them deployed</div>
+                </div>
+                <div className="flex flex-col gap-[10px]">
+                    <div className="dev-toolkit-sub-heading">Version Control</div>
+                    <div className="dev-toolkit-desc">Git, SVN, Mercurial – he’s got them committed</div>
+                </div>
+                <div className="flex flex-col gap-[10px]">
+                    <div className="dev-toolkit-sub-heading">Testing</div>
+                    <div className="dev-toolkit-desc">JUnit, Selenium, Jest – he’s got them covered</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills
