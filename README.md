eae8e14150c73414bebf6874e961980bda05c316
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-06-17 17:02:49 +0530

    init :tada:

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# portfolio
+# Transforming ideas into reality with code
+
+This is my portfolio that showcases my skills and projects as a Full Stack Sftware Engineer.
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
