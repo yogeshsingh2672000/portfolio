ad8dde3aeaec92b8184ffceadf54f5271d91d930
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-06-17 18:34:58 +0530

    about section

---
+++ src/components/sections/About.tsx
@@ -0,0 +1,14 @@
+function About() {
+    return (
+        <div className="flex flex-col md:flex-row gap-[20px] px-[20px] md:px-[40px] lg:px-[100px] md:gap-[40px] py-[60px] md:py-[80px] lg:py-[100px]">
+            <div className="meet-yk-heading whitespace-nowrap text-[32px] md:text-[48px]">Meet Yogesh Kumar</div>
+            <div className="meet-yk-desc">
+                Peek behind the code and unveil the creative genius of Yogesh Kumar, a
+                Full Stack Software Developer crafting remarkable digital experiences.
+                Spoiler alert: You’ll be amazed by what you discover!
+            </div>
+        </div>
+    );
+}
+
+export default About;
