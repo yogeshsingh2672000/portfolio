064ad9706c3bf8d0c9e54a1ceb8b29be08efcbd5
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-07-11 22:11:30 +0530

    remove unsused code

--- src/components/sections/Feedbacks.tsx
+++ src/components/sections/Feedbacks.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function Feedbacks() {
   return (
     <div id="feedback" className="bg-[#778DA9] py-[20px] md:py-[80px] lg:py-[100px] min-h-[600px] grid grid-cols-1 md:grid-cols-2 gap-[20px] px-[20px] md:px-[40px] lg:px-[100px]">
