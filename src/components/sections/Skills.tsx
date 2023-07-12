45377145a80ec3892678c3f3274a66684e034e0e
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-07-12 15:32:51 +0530

    replaced text to first person

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -7,27 +7,27 @@ function Skills() {
             <div className="grid grid-cols-1 gap-[40px] md:grid-cols-3">
                 <div className="flex flex-col gap-[10px]">
                     <div className="dev-toolkit-sub-heading">Languages</div>
-                    <div className="dev-toolkit-desc">Typescript, Javascript, Python, solidity – you name it, he's fluent</div>
+                    <div className="dev-toolkit-desc">Typescript, Javascript, Python, solidity – you name it, I'm fluent</div>
                 </div>
                 <div className="flex flex-col gap-[10px]">
                     <div className="dev-toolkit-sub-heading">Frameworks</div>
-                    <div className="dev-toolkit-desc">Node.js, React.js, Next.js, Django, Laravel, Tailwind, Bootstap, Material UI – he’s the architect</div>
+                    <div className="dev-toolkit-desc">Node.js, React.js, Next.js, Django, Laravel, Tailwind, Bootstap, Material UI – I'm the architect</div>
                 </div>
                 <div className="flex flex-col gap-[10px]">
                     <div className="dev-toolkit-sub-heading">Databases</div>
-                    <div className="dev-toolkit-desc">MySQL, PostgreSQL, MongoDB – he’s got it stored</div>
+                    <div className="dev-toolkit-desc">MySQL, PostgreSQL, MongoDB – I got it stored</div>
                 </div>
                 <div className="flex flex-col gap-[10px]">
                     <div className="dev-toolkit-sub-heading">DevOps</div>
-                    <div className="dev-toolkit-desc">GCP, AWS, Docker, Kubernetes – he’s got them deployed</div>
+                    <div className="dev-toolkit-desc">GCP, AWS, Docker, Kubernetes – I got them deployed</div>
                 </div>
                 <div className="flex flex-col gap-[10px]">
                     <div className="dev-toolkit-sub-heading">Version Control</div>
-                    <div className="dev-toolkit-desc">Git, BitBucket – he’s got them committed</div>
+                    <div className="dev-toolkit-desc">Git, BitBucket – I got them committed</div>
                 </div>
                 <div className="flex flex-col gap-[10px]">
                     <div className="dev-toolkit-sub-heading">Testing</div>
-                    <div className="dev-toolkit-desc">Jest – he’s got them covered</div>
+                    <div className="dev-toolkit-desc">Jest – I got them covered</div>
                 </div>
             </div>
         </div>
