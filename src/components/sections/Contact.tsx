5691f4f2829c2c3fa90da2f15f148bc78a4c235f
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-07-12 18:06:36 +0530

    add github & enhance tooltip looks

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -18,19 +18,22 @@ function Contact() {
     <div id="contact" className="px-[20px] md:px-[50px] lg:px-[100px] py-[20px] md:py-[80px] lg:py-[100px]">
         <div className="bg-[#778DA9] dev-toolkit-heading text-[32px] md:text-[48px]">Inspired by me? Get in touch and witness magic unfold!</div>
         <div className="flex items-center gap-[20px] pt-[40px]">
-            <Tooltip content="yogeshsingh2672000@gmail.com">
+            <Tooltip content="yogeshsingh2672000@gmail.com" className="rounded-full px-[10px]">
                 <a href="mailto:yogeshsingh2672000@gmail.com" className="flex justify-center items-center"><Button><img src="/icons/gmail.png" className="h-12 w-12" /></Button></a>
             </Tooltip>
-            <Tooltip content="yogesh-krr">
+            <Tooltip content="yogesh-krr" className="rounded-full px-[10px]">
                 <a href="https://www.linkedin.com/in/yogesh-krr/" target="_blank" className="flex justify-center items-center"><Button><img src="/icons/linkedin.png" className="h-12 w-12" /></Button></a>
             </Tooltip>
-            <Tooltip content="@YogeshKrr">
+            <Tooltip content="@YogeshKrr" className="rounded-full px-[10px]">
                 <a href="https://twitter.com/YogeshKrr" target="_blank" className="flex justify-center items-center"><Button><img src="/icons/twitter.png" className="h-8 w-12" /></Button></a>
             </Tooltip>
-            <Tooltip content={copyState}>
+            <Tooltip content="yogeshsingh2672000" className="rounded-full px-[10px]">
+                <a href="https://github.com/yogeshsingh2672000" target="_blank" className="flex justify-center items-center"><Button><img src="/icons/github.png" className="h-12 w-12" /></Button></a>
+            </Tooltip>
+            <Tooltip content={copyState} className="rounded-full px-[10px]">
                 <Button onClick={()=>{copyToClipboard("yogeshkr")}}><img src="/icons/discord.png" className="h-12 w-12" /></Button>
             </Tooltip>
-            <Tooltip content="Resume">
+            <Tooltip content="Resume" className="rounded-full px-[10px]">
                 <a href="/resume/Resume.pdf"><Button><img src="/icons/download.png" className="h-12 w-12" /></Button></a>
             </Tooltip>
         </div>
