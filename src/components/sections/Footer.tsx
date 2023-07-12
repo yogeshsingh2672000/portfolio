16e78315df21006cb9372780e4966e886d18887b
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-07-12 17:50:45 +0530

    enhanced the footer look

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <div className="grid place-items-center px-[20px] md:px-[50px] lg:px-[100px] py-[20px] md:py-[80px] lg:py-[100px]">
-      <div>demo image</div>
-      <div>&copy; 2023 Yogesh Kumar, Developer Extraordinaire</div>
+    <div className="grid place-items-center px-[20px] md:px-[50px] lg:px-[100px] pb-[20px] md:pb-[80px] lg:pb-[100px]">
+      <img className="h-40 w-40" src="/footer.png" alt="" />
+      <div className="text-center">&copy; 2023 Yogesh Kumar, Developer Extraordinaire</div>
     </div>
   )
 }
