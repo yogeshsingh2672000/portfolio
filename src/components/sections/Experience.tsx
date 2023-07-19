69f3c38f27c090fbbab5435cd3c4bd6a617a40e9
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-07-19 22:28:29 +0530

    updated Exp

--- src/components/sections/Experience.tsx
+++ src/components/sections/Experience.tsx
@@ -10,9 +10,35 @@ function Experience() {
                 <div className="bg-[#67809F] px-[10px] flex flex-col justify-between py-[20px] items-center min-w-[300px] md:min-w-[350px] max-w-[500px] w-full h-auto md:h-[500px] rounded-[20px]">
                     <div className="text-center">
                         <div className="font-bold"><span className="text-lg">SDE</span> @ Conciselabs</div>
-                        <div className="text-white">Oct 2022 - Present</div>
+                        <div className="text-white">April 2023 - Present</div>
                     </div>
-                    <div className="text-center py-[30px] md:py-0">Developed a complete project from scratch, build a <strong>Adaptive</strong> UI with a modern interface, resulting in <strong>increased</strong> user satisfaction and retention. Build and maintained a <strong>Rest</strong> <strong>API</strong> using Node.js and Express.js</div>
+                    <div className="px-[20px] py-[30px] md:py-0 overflow-y-auto hidescrollbar">
+                        <ul className='list-disc'>
+                            <li>Developed user <strong>authentication</strong> functionality, implementing secure and efficient authentication mechanisms to ensure <strong>seamless</strong> user access and authorization.</li>
+                            <li>Built REST APIs using Node.js, Express, and Firebase, enabling smooth <strong>communication</strong> between the front-end and back-end systems and facilitating data exchange.</li>
+                            <li>Wrote comprehensive <strong>test cases</strong> using Jest, ensuring thorough testing and validation of the implemented features, thereby enhancing the <strong>reliability</strong> and <strong>stability</strong> of the application.</li>
+                            <li>Created an <strong>adaptive</strong> and responsive product using <strong>React</strong>, <strong>Next.js</strong>, and Tailwind CSS, delivering an optimal user experience across various devices and screen sizes.</li>
+                            <li>Actively monitored and analyzed <strong>user feedback</strong>, closely observing user behavior and preferences to iteratively <strong>refine the design</strong> flow and improve overall user experience.</li>
+                            <li>Engaged in code reviews, diligently identifying and resolving issues, while adhering to coding standards and ensuring high code quality.</li>
+                            <li>Actively participated in team discussions and meetings, <strong>sharing innovative ideas</strong> and contributing to continuous process improvement initiatives.</li>
+                        </ul>
+                    </div>
+                    <a href="https://conciselabs.io/" target="_blank"><img className="w-8 h-8" src="/icons/exitlink.png" alt="exitlink" /></a>
+                </div>
+                <div className="bg-[#67809F] px-[10px] flex flex-col justify-between py-[20px] items-center min-w-[300px] md:min-w-[350px] max-w-[500px] w-full h-auto md:h-[500px] rounded-[20px]">
+                    <div className="text-center">
+                        <div className="font-bold"><span className="text-lg">SDE - Intern</span> @ Conciselabs</div>
+                        <div className="text-white">Oct 2022 - March 2023</div>
+                    </div>
+                    <div className="px-[20px] py-[30px] md:py-0 overflow-y-auto hidescrollbar">
+                        <ul className='list-disc'>
+                            <li>Spearheaded the development of a complete project from scratch, showcasing exceptional initiative and problem-solving skills to deliver a functional and innovative solution.</li>
+                            <li>Designed and implemented an Adaptive UI with a modern interface, leveraging cutting-edge technologies and design principles. Resulted in a remarkable 50% increase in user satisfaction and retention rates.</li>
+                            <li>Actively collaborated with cross-functional teams, playing a key role in gathering requirements, providing valuable insights, and contributing to effective project planning and execution.</li>
+                            <li>Engaged in rigorous code reviews, proactively identifying and resolving issues, while strictly adhering to coding standards. This ensured high code quality and system reliability.</li>
+                            <li>Actively participated in team discussions and meetings, sharing innovative ideas and contributing to continuous process improvement initiatives. This fostered a collaborative environment and encouraged the exploration of new ideas.</li>
+                        </ul>
+                    </div>
                     <a href="https://conciselabs.io/" target="_blank"><img className="w-8 h-8" src="/icons/exitlink.png" alt="exitlink" /></a>
                 </div>
                 <div className="bg-[#67809F] px-[10px] flex flex-col justify-between py-[20px] items-center min-w-[300px] md:min-w-[350px] max-w-[500px] w-full h-auto md:h-[500px] rounded-[20px]">
@@ -20,7 +46,16 @@ function Experience() {
                         <div className="font-bold"><span className="text-lg">SDE</span> - Intern @ Letsdressup Pvt. Ltd.</div>
                         <div className="text-white">Jan 2022 - June 2022</div>
                     </div>
-                    <div className="text-center py-[30px] md:py-0">Designed and developed complete UI for Inventory Management System using Laravel, Bootstrap, jQuery, PHP, with a customisable <strong>workflow</strong> for all the stakeholders, Inventory Manager, Designers, Workers, etc. Implemented highly scalable architecture using EC2 instances and <strong>MySQL</strong> to handle large amounts of data with low <strong>latency</strong></div>
+                    <div className="px-[20px] py-[30px] md:py-0 overflow-y-auto hidescrollbar">
+                        <ul className='list-disc'>
+                            <li>Actively contributed to discussions and brainstorming sessions, offering innovative <strong>ideas</strong> to enhance system performance and user experience.</li>
+                            <li>Implemented a <strong>customizable workflow</strong> to cater to the needs of various <strong>stakeholders</strong> including Inventory Manager, Designers, Workers, and other relevant parties.</li>
+                            <li>Ensured seamless <strong>integration</strong> of different components and functionalities within the Inventory Management System.</li>
+                            <li>Designed and developed a comprehensive <strong>user interface </strong> for an Inventory Management System using Laravel, Bootstrap, jQuery, and PHP.</li>
+                            <li>Collaborated with the team to <strong>gather</strong> requirements, analyze user needs, and deliver effective solutions.</li>
+                            <li>Participated in code reviews, debugging, and testing to <strong>maintain</strong> code quality and system reliability.</li>
+                        </ul>
+                    </div>
                     <a href="https://letsdressup.in/" target="_blank"><img className="w-8 h-8" src="/icons/exitlink.png" alt="exitlink" /></a>
                 </div>
                 <div className="bg-[#67809F] px-[10px] flex flex-col justify-between py-[20px] items-center min-w-[300px] md:min-w-[350px] max-w-[500px] w-full h-auto md:h-[500px] rounded-[20px]">
@@ -28,7 +63,13 @@ function Experience() {
                         <div className="font-bold"><span className="text-lg">Python Developer Intern</span> - Arity Technologies</div>
                         <div className="text-white">Nov 2021 - Dec 2021</div>
                     </div>
-                    <div className="text-center py-[30px] md:py-0">Developed <strong>Python</strong> script to populate the data using the Excel sheets with highly efficient way, considering all the edge cases and also implemented <strong>bulk import</strong> feature using <strong>Python</strong> and <strong>Pandas</strong></div>
+                    <div className="py-[30px] md:py-0 px-[20px] hidescrollbar">
+                        <ul className='list-disc'>
+                            <li>Designed and implemented a complete <strong>bulk insert</strong> script using Python and Pandas, optimizing the data insertion process and driving an exceptional <strong>80%</strong> improvement in efficiency.</li>
+                            <li>Demonstrated attention to detail by handling all <strong>edge cases</strong> during the development process, ensuring the script's <strong>robustness</strong> and unwavering reliability.</li>
+                            <li>Exhibited a meticulous approach by crafting comprehensive <strong>test cases</strong> to rigorously validate the functionality, performance, and stability of the Python script, guaranteeing utmost <strong>accuracy</strong>.</li>
+                        </ul>
+                    </div>
                     <img className="w-8 h-8 cursor-not-allowed opacity-[0.3]" src="/icons/exitlink.png" alt="exitlink" />
                 </div>
             </div>
