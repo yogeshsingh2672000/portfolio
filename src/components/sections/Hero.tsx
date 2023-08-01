ea9147e79934dc5095b0dfe628df8159a6787e19
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-08-01 12:50:36 +0530

    add transition on hover on nav buttons

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,11 +8,11 @@ function Hero() {
                 <div>A Full Stack Developer</div>
             </div>
             <div className='grid grid-cols-2 md:grid-cols-5 gap-[20px]'>
-                <a className="border border-[#c9c7c9] rounded-[5px] px-[15px] py-[5px]" href="#about"><button className='hero-button'>About</button></a>
-                <a className="border border-[#c9c7c9] rounded-[5px] px-[15px] py-[5px]" href="#skills"><button className='hero-button'>Skills</button></a>
-                <a className="border border-[#c9c7c9] rounded-[5px] px-[15px] py-[5px]" href="#experience"><button className='hero-button'>Experience</button></a>
-                <a className="border border-[#c9c7c9] rounded-[5px] px-[15px] py-[5px]" href="#projects"><button className='hero-button'>Projects</button></a>
-                <a className="border border-[#c9c7c9] rounded-[5px] px-[15px] py-[5px]" href="#contact"><button className='hero-button'>Contact</button></a>
+                <a className="border border-[#c9c7c9] hover:bg-[#ffffff33] transition-colors ease-in-out rounded-[5px] px-[15px] py-[5px]" href="#about"><button className='hero-button'>About</button></a>
+                <a className="border border-[#c9c7c9] hover:bg-[#ffffff33] transition-colors ease-in-out rounded-[5px] px-[15px] py-[5px]" href="#skills"><button className='hero-button'>Skills</button></a>
+                <a className="border border-[#c9c7c9] hover:bg-[#ffffff33] transition-colors ease-in-out rounded-[5px] px-[15px] py-[5px]" href="#experience"><button className='hero-button'>Experience</button></a>
+                <a className="border border-[#c9c7c9] hover:bg-[#ffffff33] transition-colors ease-in-out rounded-[5px] px-[15px] py-[5px]" href="#projects"><button className='hero-button'>Projects</button></a>
+                <a className="border border-[#c9c7c9] hover:bg-[#ffffff33] transition-colors ease-in-out rounded-[5px] px-[15px] py-[5px]" href="#contact"><button className='hero-button'>Contact</button></a>
             </div>
         </div>
     )
